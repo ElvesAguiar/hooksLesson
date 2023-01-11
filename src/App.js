d5b53ebbfc3c034ae9d7f35007fdbb9c9e4ecfc8
Elves Aguiar <elvesmhs@hotmail.com>
2023-01-11 20:50:51 -0300

    refactor: hooks usecallback and memo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React, { useState } from 'react';
- 
+import React,{useState,useEffect, useMemo, useCallback } from "react";
+
 function App() {
- 
-  // Declarar uma nova variável de state, na qual chamaremos de "contador"
-  const [count, setContador] = useState(0);
- 
+
+  const [task, setTask] = useState(['Pagar a conta de luz',
+        'estudar react hooks']);
+      
+  const [name, setName] = useState('Matias', 'Vih0', 'karol');
+  const [input, setInput] =  useState('');
+
+
+  useEffect(()=>{
+    const storageTasks = localStorage.getItem('task');
+    if(storageTasks){
+      setTask(JSON.parse(storageTasks));
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('task',JSON.stringify(task));
+  }, [task]);
+
+  const handleAdd= useCallback(()=>{
+    setTask([...task, input]);
+    setInput('');
+  
+  },[input,task]);
+
+  const totalTask = useMemo(() => task.length, [task]);
+
+
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setContador(count + 1)}>
-          Aumentar
-      </button>
+     <ul>
+      {task.map(task=>(<li key={task} > {task} </li>))}
+     </ul>
+    <br/>
+
+    <strong>vc tem {totalTask} tarefas</strong><br/>
+
+    <input type="text" value={input} onChange={(event => setInput(event.target.value))}/>
+
+    <button type="button" onClick={handleAdd}>Adicionar</button>
+
     </div>
   );
-}
+};
 
 export default App;
